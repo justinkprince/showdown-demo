5c6c09ca6636df3570f69bd0d64738b8ed01f806
Justin Prince <jprince@advmicrogid.com>
2019-10-09 19:04:23 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import showdown from 'showdown';
+
+const converter = new showdown.Converter();
+const markdown = `
+# Some headline
+
+* Some list
+* Another list item
+`;
+
+const html = converter.makeHtml(markdown);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div dangerouslySetInnerHTML={{__html: html}} className="content">
+      </div>
     </div>
   );
 }
